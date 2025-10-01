87c1091a1ff3c2a73fcca63c2c4b04663b9d456e
shoobooshooboo <johnonymous4@gmail.com>
2025-10-01 15:00:09 -0700

    started detailing the Battle implementation

--- battle-simulator/src/battle.rs
+++ battle-simulator/src/battle.rs
@@ -3,11 +3,19 @@ use bevy::prelude::*;
 pub mod combatant;
 use combatant::Combatant;
 
+type CombatantList = Vec<Box<dyn Combatant>>;
+
 pub struct Battle{
     //controlled by player
-    players: Vec<Box<dyn Combatant>>,
+    players: CombatantList,
     //not controlled by player but on player-side
-    allies: Vec<Box<dyn Combatant>>,
+    allies: CombatantList,
     //not controlled by player and not on player-side.
-    enemies: Vec<Box<dyn Combatant>>,
+    enemies: CombatantList,
+}
+
+impl Battle{
+    pub fn new() -> Battle{}
+    pub fn get_players(&self) -> &CombatantList{}
+    pub fn get_players_mut(&mut self) -> &mut CombatantList{}
 }
